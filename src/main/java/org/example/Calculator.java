0bebee5e5c4faae9221906ad46a248e41efd7b3a
Justinmlnr <juju.vlb38@gmail.com>
2026-03-24 11:35:40 +0100

    Partie1

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Calculator {
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public int soustraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/TestM.java
@@ -0,0 +1,37 @@
+import org.example.Calculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestM {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAddition() {
+        int result = calculator.addition(10, 15);
+        Assertions.assertEquals(25,result);
+    }
+
+    @Test
+    void testSoustraction() {
+        int result = calculator.soustraction(15,10);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    void testMultiplication() {
+        int result = calculator.multiplication(5, 5);
+        Assertions.assertEquals(25,result);
+    }
+
+    @Test
+    void testDivision () {
+        double result = calculator.division(35,5);
+        Assertions.assertEquals(7, result);
+    }
+
+    @Test
+    void testMultiplicationBig() {
+        int result = calculator.multiplication(300000, 300000);
+        Assertions.assertEquals(90000000000,result);
+    }
+}
